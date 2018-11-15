51131abeff637bafcb14a803104a06361e02b554
walfud <hi@walfud.com>
2018-11-15 19:25:48 +0800

    toast

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,24 @@
 <template>
   <div id="app">
-    Hello
+     <button @click="toast">Show Native Toast</button>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App',
-}
+  name: "App",
+  methods: {
+    toast: function() {
+      const { Toast } = NativeAPI
+      Toast.show('Hello Yun App', Toast.LENGTH_SHORT)
+    }
+  }
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
